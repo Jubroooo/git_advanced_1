5eb2c00ea6df34e69b3d8f091c068c72b0bfddf5
jubro <ljh06050605@gmail.com>
2024-04-18 19:11:06 +0900

    init main

---
+++ main.py
@@ -0,0 +1,35 @@
+from typing import List
+
+#Skeleton code for even_list
+def even_list(int_list:List[int]) -> List[int]:
+    """
+    Determines if a number is even and return an even list.
+    
+    Args:
+        int_list: A list of integer.
+    """
+    #TODO: Implement sum_of_squares_of_even
+    pass 
+
+#Skeleton code for sum_of_squares_of_even
+def sum_of_squares_of_even(int_list:List[int]) -> List[int]:
+    """
+    Computes the sum of squares of all even numbers in a list.
+    
+    Args:
+        TThe sum of the squares of all even numbers in the list.
+    """
+    #TODO: Implement sum_of_squares_of_even
+    pass      
+
+#Main function
+def main():
+    #Example list
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_list_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_list_list)
+    print(output)
+    
+#Boilerplate code
+if __name__=="__main__":
+    main()
